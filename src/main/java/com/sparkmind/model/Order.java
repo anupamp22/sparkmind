a6dc189d33494729b220435536b721b2d1993751
Anupam Priyadarshi <anupam.p22@gmail.com>
2014-05-06 17:37:48 -0700

    committing all the changes until 5/6

--- src/main/java/com/sparkmind/model/Order.java
+++ src/main/java/com/sparkmind/model/Order.java
@@ -18,6 +18,7 @@ import java.util.Set;
  */
 @Entity
 @Table(name="order", schema="test")
+//@Table(name="order", schema="public")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/sparkmind/service/ProductConfigServiceImpl.java
+++ src/main/java/com/sparkmind/service/ProductConfigServiceImpl.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,39 +21,45 @@ public class ProductConfigServiceImpl implements ProductConfigService {
     @PersistenceContext
     EntityManager em;
 	
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public List<Category> getAllCategories(){
 		Query q = em.createQuery("FROM Category");
 		return q.getResultList();
 	}
 	
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public List<Product> getAllProducts(){
 		Query q = em.createQuery("FROM Product");
 		return q.getResultList();
 	}
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public Category getCategoryByName(String name){
 		Category p = (Category)em.createQuery("FROM Category p where p.name=:name").setParameter("name", name).getSingleResult();
 		System.out.println(p);
 		return p;
 	}
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public Product getProductByName(String name){
 		Product p = (Product)em.createQuery("FROM PRODUCT p where p.name=:name").setParameter("name", name).getSingleResult();
 		return p;
 	}
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public Category getCategoryById(int id){
 		Category c = em.find(Category.class, id);
 		return c;
 	}
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
 	public Product getProductById(int id){
 		Product p = em.find(Product.class, id);
 		return p;
 	}
-    
+    @Override
     @Transactional
     public void removeCategory(int id){
     	Category c = em.find(Category.class, id);
@@ -60,6 +67,7 @@ public class ProductConfigServiceImpl implements ProductConfigService {
     		em.remove(c);
     	}    	
     }
+    @Override
     @Transactional
     public void removeProduct(int id){
     	Product p = em.find(Product.class, id);
@@ -67,29 +75,31 @@ public class ProductConfigServiceImpl implements ProductConfigService {
     		em.remove(p);
     	}
     }
-    
+    @Override
     @Transactional
     public void saveCategory(Category c){
     	em.persist(c);
     }
+    @Override
     @Transactional
     public void saveProduct(Product p){
     	em.persist(p);
     }
-    
+    @Override
     @Transactional
     public void saveOrder(Order o){
     	em.persist(o);		
     	
     }
-    
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
+    @Cacheable(value = "orders")
     public List<Order> getOrdersByUserId(Long userId){
     	Query q = em.createQuery("FROM Order o where o.userId=:userId").setParameter("userId", userId);
     	return q.getResultList();
     }
-    
-    @Transactional
+    @Override
+    @Transactional(readOnly = true)
     public Order getProductsForOrder(int orderId){
     	Query q = em.createQuery("SELECT distinct o FROM Order o LEFT JOIN FETCH o.productList where o.id=:orderId");
     	q.setParameter("orderId", orderId);
